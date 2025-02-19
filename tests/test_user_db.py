7d2cbe51f8d7d1b964fc3507e6af603dd99b4645
Francisco Santarelli <franciscosantarelli@gmail.com>
2025-02-19 01:06:10 -0300

    Add test user db action

--- tests/test_user_db.py
+++ tests/test_user_db.py
@@ -3,7 +3,7 @@ from repositories.users_repository import UsersRepository
 from werkzeug.security import check_password_hash
 from models import Users
 
-def test_register_db(db_session, valid_user):
+def test_register_user_db(db_session, valid_user):
     username = valid_user.get('username')
     password = valid_user.get('password')
     new_user = UsersRepository().register(username=username, password=password)
@@ -11,3 +11,15 @@ def test_register_db(db_session, valid_user):
     assert new_user.password != password
     assert isinstance(new_user, Users)
     assert check_password_hash(new_user.password, password) == True
+
+def test_get_user_by_username(db_session, valid_user):
+    username = valid_user.get('username')
+    password = valid_user.get('password')
+    existing_user = UsersRepository().get_user_by_username(username=username)
+    assert existing_user is None
+    new_user = UsersRepository().register(username=username, password=password)
+    existing_user = UsersRepository().get_user_by_username(username=username)
+    assert existing_user.username == username
+    assert existing_user.password != password
+    assert isinstance(existing_user, Users)
+    assert check_password_hash(existing_user.password, password) == True
